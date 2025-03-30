f8f9f660bd84d76fd11ddb6784a96323ff81adeb
KinichJueMe <aas0309@163.com>
2025-03-30 15:54:18 +0800

    添加创建类功能

--- durian.py
+++ durian.py
@@ -1,8 +1,24 @@
 import sys
+import link
 
 def main():
-    pass
-
-if __name__ == "__main__":
     # 获取命令行数据
     x = sys.argv[1::]
+    # 判断对应功能
+    if x[0] == "NEW":  # 功能分支-创建
+        if x[1] == "CLASS":  # 功能分支-类
+            # 数据（完整）：
+            # NEW CLASS <类的名字>
+            #  0     1      2
+            link.Class.new(x[2])
+
+    elif x[0] == "INIT":  # 功能分支-初始化
+        # 初始化仓库
+        link.Init()
+        link.Logger.info("初始化仓库完成！")
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        link.logger.error(f"主线程崩溃！=> {str(e)}")
